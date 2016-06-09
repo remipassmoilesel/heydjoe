cc327fc44abc1eb24f6395b80029188bd539fe8f
remi.passmoilesel <remi.passmoilesel@gmail.com>
2016-06-09 18:01:22 +0200

    Invite user on list

--- var.www.djoe/jsxc/dev/jsxc.js
+++ var.www.djoe/jsxc/dev/jsxc.js
@@ -10894,7 +10894,8 @@ jsxc.xmpp.search = {
      * @returns {boolean}
      */
     isUserSearchAvailable: function () {
-        return this.userSearchAvailable;
+        var self = jsxc.xmpp.search;
+        return self.userSearchAvailable;
     },
 
     /**
