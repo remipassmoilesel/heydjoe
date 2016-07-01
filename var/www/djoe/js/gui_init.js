3ba5d476e45fd694d6e114bafe85875214976861
remi.passmoilesel <remi.passmoilesel@gmail.com>
2016-07-01 23:10:30 +0200

    Show GUI on connection

--- var/www/djoe/js/gui_init.js
+++ var/www/djoe/js/gui_init.js
@@ -1,181 +1,180 @@
-$(function () {
+$(function() {
 
-    // page domain
-    var pageDomain = "im.silverpeas.net";
+  // page domain
+  var pageDomain = "im.silverpeas.net";
 
-    // xmpp domain
-    var xmppDomain = "im.silverpeas.net";
+  // xmpp domain
+  var xmppDomain = "im.silverpeas.net";
 
-    var defaultPassword = "azerty";
+  var defaultPassword = "azerty";
 
-    /**
-     Logins disponibles
-     */
-    var availablesLogins = [
-        "remi",
-        "david",
-        "yohann",
-        "miguel",
-        "aurore",
-        "nicolas",
-        "sebastien"
-    ];
+  /**
+   Logins disponibles
+   */
+  var availablesLogins = ["remi", "david", "yohann", "miguel", "aurore", "nicolas", "sebastien"];
 
-    var webAdminUrl = "http://" + pageDomain + ":9090/";
-    var consoleAdminUrl = "http://" + pageDomain + ":9091/";
-    var etherpadUrl = "http://" + pageDomain + ":9001/";
-    var consoleUrl = "https://" + pageDomain + "/xmpp-console/";
-    var discoUrl = "https://" + pageDomain + "/xmpp-disco/";
+  var webAdminUrl = "http://" + pageDomain + ":9090/";
+  var consoleAdminUrl = "http://" + pageDomain + ":9091/";
+  var etherpadUrl = "http://" + pageDomain + ":9001/";
+  var consoleUrl = "https://" + pageDomain + "/xmpp-console/";
+  var discoUrl = "https://" + pageDomain + "/xmpp-disco/";
 
-    var xmppResource = "heyDjoe";
+  var xmppResource = "heyDjoe";
 
-    // Initialize consoles
-    $("#eventsConsole").eventConsole();
-    new StorageConsole("storageConsole");
+  // Initialize consoles
+  $("#eventsConsole").eventConsole();
+  new StorageConsole("storageConsole");
 
-    $(document).on("attached.jsxc ", function () {
-        $("#xmppInspector").xmppInspector(jsxc.xmpp.conn);
-    });
+  $(document).on("attached.jsxc ", function() {
+    $("#xmppInspector").xmppInspector(jsxc.xmpp.conn);
+  });
 
-    // index tabs
-    $("#tabs").tabs();
+  // index tabs
+  $("#tabs").tabs();
 
-    // Listen connexion fail
-    $(document).on("authfail.jsxc ", function () {
-        jsxc.xmpp.logout(true);
-        $("#feedbackArea").html("<b>Echec de la connexion. Rechargez la page puis rééssayez !</b>");
-    });
+  // Listen connexion fail
+  $(document).on("authfail.jsxc ", function() {
+    jsxc.xmpp.logout(true);
+    $("#feedbackArea").html("<b>Echec de la connexion. Rechargez la page puis rééssayez !</b>");
+  });
 
-    // listen connexion success
-    $(document).on("attached.jsxc ", function () {
-        $("#feedbackArea").html("<i>Connexion établie</i>");
-    });
+  // listen connexion success
+  $(document).on("attached.jsxc ", function() {
+    $("#feedbackArea").html("<i>Connexion établie</i>");
+  });
 
-    // random login
-    function showRandomName() {
-        $("#randomLogin").val((chance.first() + "_" + chance.last()).toLowerCase());
-    }
+  // random login
+  function showRandomName() {
+    $("#randomLogin").val((chance.first() + "_" + chance.last()).toLowerCase());
+  }
 
-    $("#refreshRandomLogin").click(function () {
-        showRandomName();
-        $(".connexionForm input[value='random']").trigger("click");
-    });
+  $("#refreshRandomLogin").click(function() {
     showRandomName();
+    $(".connexionForm input[value='random']").trigger("click");
+  });
+  showRandomName();
 
-    $("#refreshRandomLogin").click(function () {
-        showRandomName();
-        $(".connexionForm input[value='random']").trigger("click");
-    });
+  $("#refreshRandomLogin").click(function() {
+    showRandomName();
+    $(".connexionForm input[value='random']").trigger("click");
+  });
 
-    $("#predefinedJidList").click(function () {
-        $(".connexionForm input[value='predefined']").trigger("click");
-    });
+  $("#predefinedJidList").click(function() {
+    $(".connexionForm input[value='predefined']").trigger("click");
+  });
 
-    $("#randomLogin").keyup(function () {
-        $(".connexionForm input[value='random']").trigger("click");
-    });
+  $("#randomLogin").keyup(function() {
+    $(".connexionForm input[value='random']").trigger("click");
+  });
 
-    // connexion button
-    $('#connectButton').click(function () {
+  // connexion button
+  $('#connectButton').click(function() {
 
-        var userNode;
+    var userNode;
 
-        if ($(".connexionForm input[value='random']:checked").length === 1) {
-            userNode = $("#randomLogin").val();
-        }
-
-        else {
-            userNode = $("#predefinedJidList").val();
-        }
-
-        userNode = userNode.trim().toLowerCase();
-        var userJid = userNode + "@" + xmppDomain;
-
-        jsxc.rest.openfire.createUser(userNode)
-
-            .then(function () {
-
-                // connexion et lancement du GUI
-                jsxc.start(userJid, defaultPassword);
-
-            }, function (response) {
-
-                // acceptable codes: created, exist,
-                var codes = [201, 409];
-
-                // user already exist, connexion
-                if (codes.indexOf(response.status) !== -1) {
-                    jsxc.start(userJid, defaultPassword);
-                }
-
-                // other fail
-                else {
-                    console.error("Fail creating chat user");
-                    console.error(response);
-                    $("#feedbackArea").html("<i>Erreur lors de la creation de l'utilisateur. Veuillez rafraichir la page et réessayer ! (code: " + (response.status || "indéfini") + "</i>");
-                }
-
-            });
-
-    });
-
-    // disconnect button
-    $('#disconnectButton').click(function () {
-        jsxc.xmpp.logout(true);
-    });
-
-    // create pad
-    $('#newPadButton').click(function () {
-        window.open(etherpadUrl + "p/" + $("#newPadName").val(), '_blank');
-    });
-
-    // show availables accounts
-    for (var i = 0; i < availablesLogins.length; i++) {
-        var lg = availablesLogins[i];
-
-        // les ajouter à la liste de sélection de pseudo
-        $("#predefinedJidList").append("<option value='" + lg + "'>" + lg + "</option>");
+    if ($(".connexionForm input[value='random']:checked").length === 1) {
+      userNode = $("#randomLogin").val();
     }
 
-    // utils and informations
-    var appendToUtilsInfo = function (name, elmt) {
-        $('#infoDisplay').append("<tr><td>" + (name || '') + "</td><td>" + elmt + "</td><tr/>");
-    };
+    else {
+      userNode = $("#predefinedJidList").val();
+    }
 
-    appendToUtilsInfo('Feuille de route',
-        '<a target="_blank" href="http://' + pageDomain + ':9001/p/feuille-de-route">http://' + pageDomain + ':9001/p/feuille-de-route</a>');
+    userNode = userNode.trim().toLowerCase();
+    var userJid = userNode + "@" + xmppDomain;
 
-    appendToUtilsInfo('Activité du serveur',
-        '<a target="_blank" href="http://im.silverpeas.net:8080/monitorix-cgi/monitorix.cgi?mode=localhost&graph=all&when=1day&color=black">http://im.silverpeas.net:8080/monitorix-cgi/...</a>');
+    jsxc.rest.openfire.createUser(userNode)
 
-    appendToUtilsInfo('Console XMPP',
-        '<a target="_blank" href="' + consoleUrl + '">' + consoleUrl + '</a>');
+        .then(function() {
 
-    appendToUtilsInfo('Découverte de services XMPP',
-        '<a target="_blank" href="' + discoUrl + '">' + discoUrl + '</a>');
+          // connexion et lancement du GUI
+          jsxc.start(userJid, defaultPassword);
 
-    appendToUtilsInfo('API REST Openfire',
-        '<a target="_blank" href="openfire-rest/">openfire-rest/</a>');
+          jsxc.gui.roster.toggle('shown');
 
-    appendToUtilsInfo('Domaine utilisé', pageDomain);
+        }, function(response) {
 
-    appendToUtilsInfo('Domaine XMPP utilisé', xmppDomain);
+          // acceptable codes: created, exist,
+          var codes = [201, 409];
 
-    appendToUtilsInfo('Nom XMPP du client', xmppResource);
+          // user already exist, connexion
+          if (codes.indexOf(response.status) !== -1) {
+            jsxc.start(userJid, defaultPassword);
 
-    appendToUtilsInfo('Administration web Openfire',
-        '<a target="_blank" href="' + webAdminUrl + '">' + webAdminUrl + '</a>');
+            jsxc.gui.roster.toggle('shown');
+          }
 
-    appendToUtilsInfo('Administration console Openfire',
-        '<a target="_blank" href="' + consoleAdminUrl + '">' + consoleAdminUrl + '</a>');
+          // other fail
+          else {
+            console.error("Fail creating chat user");
+            console.error(response);
+            $("#feedbackArea").html(
+                "<i>Erreur lors de la creation de l'utilisateur. Veuillez rafraichir la page et réessayer ! (code: " +
+                (response.status || "indéfini") + "</i>");
+          }
 
-    appendToUtilsInfo('Etherpad',
-        '<a target="_blank" href="' + etherpadUrl + '">' + etherpadUrl + '</a>');
+        });
 
-    appendToUtilsInfo('Wiki JSXC',
-        '<a target="_blank" href="https://github.com/jsxc/jsxc/wiki">https://github.com/jsxc/jsxc/wiki</a>');
+  });
 
-    appendToUtilsInfo('Liste de ressources',
-        '<a target="_blank" href="https://docs.google.com/spreadsheets/d/1qDF4yB3Tpd9Red2sYfCgnISfMBvddke5pYTrwUThyN8/edit#gid=365481387">https://docs.google.com/spreadsheets/...</a>');
+  // disconnect button
+  $('#disconnectButton').click(function() {
+    jsxc.xmpp.logout(true);
+  });
+
+  // create pad
+  $('#newPadButton').click(function() {
+    window.open(etherpadUrl + "p/" + $("#newPadName").val(), '_blank');
+  });
+
+  // show availables accounts
+  for (var i = 0; i < availablesLogins.length; i++) {
+    var lg = availablesLogins[i];
+
+    // les ajouter à la liste de sélection de pseudo
+    $("#predefinedJidList").append("<option value='" + lg + "'>" + lg + "</option>");
+  }
+
+  // utils and informations
+  var appendToUtilsInfo = function(name, elmt) {
+    $('#infoDisplay').append("<tr><td>" + (name || '') + "</td><td>" + elmt + "</td><tr/>");
+  };
+
+  appendToUtilsInfo('Feuille de route',
+      '<a target="_blank" href="http://' + pageDomain + ':9001/p/feuille-de-route">http://' +
+      pageDomain + ':9001/p/feuille-de-route</a>');
+
+  appendToUtilsInfo('Activité du serveur',
+      '<a target="_blank" href="http://im.silverpeas.net:8080/monitorix-cgi/monitorix.cgi?mode=localhost&graph=all&when=1day&color=black">http://im.silverpeas.net:8080/monitorix-cgi/...</a>');
+
+  appendToUtilsInfo('Console XMPP',
+      '<a target="_blank" href="' + consoleUrl + '">' + consoleUrl + '</a>');
+
+  appendToUtilsInfo('Découverte de services XMPP',
+      '<a target="_blank" href="' + discoUrl + '">' + discoUrl + '</a>');
+
+  appendToUtilsInfo('API REST Openfire',
+      '<a target="_blank" href="openfire-rest/">openfire-rest/</a>');
+
+  appendToUtilsInfo('Domaine utilisé', pageDomain);
+
+  appendToUtilsInfo('Domaine XMPP utilisé', xmppDomain);
+
+  appendToUtilsInfo('Nom XMPP du client', xmppResource);
+
+  appendToUtilsInfo('Administration web Openfire',
+      '<a target="_blank" href="' + webAdminUrl + '">' + webAdminUrl + '</a>');
+
+  appendToUtilsInfo('Administration console Openfire',
+      '<a target="_blank" href="' + consoleAdminUrl + '">' + consoleAdminUrl + '</a>');
+
+  appendToUtilsInfo('Etherpad',
+      '<a target="_blank" href="' + etherpadUrl + '">' + etherpadUrl + '</a>');
+
+  appendToUtilsInfo('Wiki JSXC',
+      '<a target="_blank" href="https://github.com/jsxc/jsxc/wiki">https://github.com/jsxc/jsxc/wiki</a>');
+
+  appendToUtilsInfo('Liste de ressources',
+      '<a target="_blank" href="https://docs.google.com/spreadsheets/d/1qDF4yB3Tpd9Red2sYfCgnISfMBvddke5pYTrwUThyN8/edit#gid=365481387">https://docs.google.com/spreadsheets/...</a>');
 
 });
--- var/www/djoe/jsxc/jsxc_init.js
+++ var/www/djoe/jsxc/jsxc_init.js
@@ -1,215 +1,215 @@
-$(function () {
+$(function() {
 
-    "use strict";
+  "use strict";
 
-    console.log("Initializing instant messaging");
+  console.log("Initializing instant messaging");
 
-    var defaultPassword = "azerty";
+  var defaultPassword = "azerty";
 
-    // page domain
-    var pageDomain = document.domain;
+  // page domain
+  var pageDomain = document.domain;
 
-    // xmpp over http url
-    var boshUrl;
-    // xmpp server domain
-    var xmppDomain;
-    // domain for search (XEP 0055)
-    var searchDomain;
+  // xmpp over http url
+  var boshUrl;
+  // xmpp server domain
+  var xmppDomain;
+  // domain for search (XEP 0055)
+  var searchDomain;
 
-    // dev mode
-    if (pageDomain === "localhost" || pageDomain === "127.0.0.1") {
-        boshUrl = "https://im.silverpeas.net/http-bind/";
-        xmppDomain = "im.silverpeas.net";
-        searchDomain = "search." + "im.silverpeas.net";
+  // dev mode
+  if (pageDomain === "localhost" || pageDomain === "127.0.0.1") {
+    boshUrl = "https://im.silverpeas.net/http-bind/";
+    xmppDomain = "im.silverpeas.net";
+    searchDomain = "search." + "im.silverpeas.net";
+  }
+
+  // normal mode
+  else {
+    boshUrl = "https://" + pageDomain + "/http-bind/";
+    xmppDomain = pageDomain;
+    searchDomain = "search." + pageDomain;
+  }
+
+  // etherpad resource
+  var etherpadRes = "https://im.silverpeas.net/etherpad/";
+
+  // clientName
+  var xmppResource = "heyDjoe";
+
+  // jsxc debug mode
+  //jsxc.storage.setItem('debug', true)
+  jsxc.storage.setItem('debug', false)
+
+  // afficher les erreurs de Strophe, indispensable
+  // var stLogLevel = Strophe.LogLevel.INFO;
+  var stLogLevel = Strophe.LogLevel.WARN;
+
+  Strophe.log = function(level, msg) {
+    if (level >= stLogLevel) {
+      console.error("Strophe [" + level + "] " + msg);
+      console.error((new Error()).stack);
     }
+  };
 
-    // normal mode
-    else {
-        boshUrl = "https://" + pageDomain + "/http-bind/";
-        xmppDomain = pageDomain;
-        searchDomain = "search." + pageDomain;
-    }
+  function silverpeasConnexion() {
 
-    // etherpad resource
-    var etherpadRes = "https://im.silverpeas.net/etherpad/";
+    var userNode = $.cookie("svpLogin").toLowerCase().trim();
+    var userJid = userNode + "@" + xmppDomain;
 
-    // clientName
-    var xmppResource = "heyDjoe";
+    /**
+     * Create eventually an user if not existing and connect him.
+     */
+    var createUserAndConnect = function() {
 
-    // jsxc debug mode
-    //jsxc.storage.setItem('debug', true)
-    jsxc.storage.setItem('debug', false)
+      jsxc.rest.openfire.createUser(userNode)
 
-    // afficher les erreurs de Strophe, indispensable
-    // var stLogLevel = Strophe.LogLevel.INFO;
-    var stLogLevel = Strophe.LogLevel.WARN;
+          .then(function() {
 
-    Strophe.log = function (level, msg) {
-        if (level >= stLogLevel) {
-            console.error("Strophe [" + level + "] " + msg);
-            console.error((new Error()).stack);
-        }
-    };
+            // connexion et lancement du GUI
+            jsxc.start(userJid, defaultPassword);
 
-    function silverpeasConnexion() {
+            jsxc.gui.roster.toggle('shown');
 
-        var userNode = $.cookie("svpLogin").toLowerCase().trim();
-        var userJid = userNode + "@" + xmppDomain;
+          }, function(response) {
 
-        /**
-         * Create eventually an user if not existing and connect him.
-         */
-        var createUserAndConnect = function () {
+            // acceptable codes: created, exist,
+            var codes = [201, 409];
 
-            jsxc.rest.openfire.createUser(userNode)
+            // not a fatal error, connexion
+            if (codes.indexOf(response.status) !== -1) {
+              jsxc.start(userJid, defaultPassword);
 
-                .then(function () {
-
-                    // connexion et lancement du GUI
-                    jsxc.start(userJid, defaultPassword);
-
-                }, function (response) {
-
-                    // acceptable codes: created, exist,
-                    var codes = [201, 409];
-
-                    // not a fatal error, connexion
-                    if (codes.indexOf(response.status) !== -1) {
-                        jsxc.start(userJid, defaultPassword);
-                    }
-
-                    // other fail
-                    else {
-                        console.error("Fail creating chat user");
-                        console.error(response);
-                    }
-
-                });
-
-        };
-
-
-        // check if not connected with another account
-        if (localStorage && jsxc && jsxc.storage && jsxc.storage.getItem && jsxc.storage.getItem('jid')) {
-
-            var pNode = Strophe.getNodeFromJid(jsxc.storage.getItem('jid'));
-
-            // need to be improved
-            if (pNode.toLowerCase() !== userNode) {
-
-                console.log("Connecting with another profile, clearing data storage.");
-                console.log(pNode, userNode);
-
-                jsxc.xmpp.logout(true);
-
-                localStorage.clear();
-
-                setTimeout(createUserAndConnect, 700);
+              jsxc.gui.roster.toggle('shown');
             }
 
+            // other fail
             else {
-
-                console.log("Connexion");
-
-                createUserAndConnect();
+              console.error("Fail creating chat user");
+              console.error(response);
             }
 
-        }
-
-        else {
-
-            console.log("Connexion");
-
-            createUserAndConnect();
-
-        }
-
-
-    }
-
-    // Initialize JSXC
-    var options = {
-
-        // REST support
-        rest: {
-            apiName: "openfire",
-            apiBaseUrl: "https://im.silverpeas.net/openfire-rest",
-            apiKey: "ztR2yJWNRu9ffPIw"
-        },
-
-        // enable Etherpad support
-        etherpad: {
-            enabled: true,
-            ressource: etherpadRes
-        },
-
-        // MUST specify muc server to avoid errors
-        muc: {
-            server: "conference.im.silverpeas.net"
-        },
-
-        favicon: {
-            enable: false,
-        },
-
-        // xmpp options
-        xmpp: {
-            url: boshUrl,
-            domain: xmppDomain,
-            resource: xmppResource,
-            overwrite: true,
-            searchDomain: searchDomain,
-        },
-
-        // disable otr because of display disturbing
-        otr: {
-            enable: false
-        },
-
-        // if 404 errors precise jsxc root
-        root: 'jsxc/',
-
-        // RTCPeerConfiguration used for audio/video calls.
-        RTCPeerConfig: {
-
-            /** Time-to-live for config from url */
-            ttl: 3600,
-
-            /** [optional] If set, jsxc requests and uses RTCPeerConfig from this url */
-            url: null,
-
-            /** If true, jsxc send cookies when requesting RTCPeerConfig from the url above */
-            withCredentials: false,
-
-            /** ICE servers like defined in http://www.w3.org/TR/webrtc/#idl-def-RTCIceServer */
-            iceServers: [
-
-                {urls: "stun:turn1.silverpeas.net:80"},
-
-                {
-                    urls: "turns:turn1.silverpeas.net:443",
-                    credential: "orv227EAdGEZ_ldSMadxhmbSxtnmvnMx",
-                    credentialType: "password",
-                    username: "djoe"
-                }
-            ]
-        }
+          });
 
     };
 
+    // check if not connected with another account
+    if (localStorage && jsxc && jsxc.storage && jsxc.storage.getItem &&
+        jsxc.storage.getItem('jid')) {
 
-    if ($.cookie && $.cookie("svpLogin")) {
-        options.callbacks = {
-            reconnectCb: silverpeasConnexion
-        };
+      var pNode = Strophe.getNodeFromJid(jsxc.storage.getItem('jid'));
+
+      // need to be improved
+      if (pNode.toLowerCase() !== userNode) {
+
+        console.log("Connecting with another profile, clearing data storage.");
+        console.log(pNode, userNode);
+
+        jsxc.xmpp.logout(true);
+
+        localStorage.clear();
+
+        setTimeout(createUserAndConnect, 700);
+      }
+
+      else {
+
+        console.log("Connexion");
+
+        createUserAndConnect();
+      }
+
     }
 
-    jsxc.init(options);
-    
-    // auto startup on silverpeas
-    if ($.cookie && $.cookie("svpLogin")) {
-        silverpeasConnexion();
+    else {
+
+      console.log("Connexion");
+
+      createUserAndConnect();
+
     }
+
+  }
+
+  // Initialize JSXC
+  var options = {
+
+    // REST support
+    rest : {
+      apiName : "openfire",
+      apiBaseUrl : "https://im.silverpeas.net/openfire-rest",
+      apiKey : "ztR2yJWNRu9ffPIw"
+    },
+
+    // enable Etherpad support
+    etherpad : {
+      enabled : true, ressource : etherpadRes
+    },
+
+    // MUST specify muc server to avoid errors
+    muc : {
+      server : "conference.im.silverpeas.net"
+    },
+
+    favicon : {
+      enable : false,
+    },
+
+    // xmpp options
+    xmpp : {
+      url : boshUrl,
+      domain : xmppDomain,
+      resource : xmppResource,
+      overwrite : true,
+      searchDomain : searchDomain,
+    },
+
+    // disable otr because of display disturbing
+    otr : {
+      enable : false
+    },
+
+    // if 404 errors precise jsxc root
+    root : 'jsxc/',
+
+    // RTCPeerConfiguration used for audio/video calls.
+    RTCPeerConfig : {
+
+      /** Time-to-live for config from url */
+      ttl : 3600,
+
+      /** [optional] If set, jsxc requests and uses RTCPeerConfig from this url */
+      url : null,
+
+      /** If true, jsxc send cookies when requesting RTCPeerConfig from the url above */
+      withCredentials : false,
+
+      /** ICE servers like defined in http://www.w3.org/TR/webrtc/#idl-def-RTCIceServer */
+      iceServers : [
+
+        {urls : "stun:turn1.silverpeas.net:80"},
+
+        {
+          urls : "turns:turn1.silverpeas.net:443",
+          credential : "orv227EAdGEZ_ldSMadxhmbSxtnmvnMx",
+          credentialType : "password",
+          username : "djoe"
+        }]
+    }
+
+  };
+
+  if ($.cookie && $.cookie("svpLogin")) {
+    options.callbacks = {
+      reconnectCb : silverpeasConnexion
+    };
+  }
+
+  jsxc.init(options);
+
+  // auto startup on silverpeas
+  if ($.cookie && $.cookie("svpLogin")) {
+    silverpeasConnexion();
+  }
 
 });
 
