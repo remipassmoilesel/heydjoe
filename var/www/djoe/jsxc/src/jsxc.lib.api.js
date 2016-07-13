85a93436b6ffe24260bd711ebfbb3fce0b3d1f00
remi.passmoilesel <remi.passmoilesel@gmail.com>
2016-07-13 14:06:53 +0200

    Add extensions to jsxc

---
+++ var/www/djoe/jsxc/src/jsxc.lib.api.js
@@ -0,0 +1,12 @@
+/**
+ * API for manipulating JSXC
+ *
+ */
+
+jsxc.api = {
+
+  openChatWindow : function(login) {
+    console.log(login);
+  },
+
+};
---
+++ var/www/djoe/jsxc/src/jsxc.lib.help.js
@@ -0,0 +1,35 @@
+/**
+ * API for manipulating JSXC
+ *
+ */
+
+jsxc.help = {
+
+  tutorials : {},
+
+  init: function(){
+
+    var self = jsxc.help;
+
+    self.tutorials["videoconference"] = function(){
+
+    };
+
+    self.tutorials["conversation"] = function(){
+
+    };
+
+
+
+  },
+
+  /**
+   * Launch a visual tutorial
+   * @param name
+   */
+  launchTutorial: function(name){
+    console.log(name);
+
+  }
+
+};
