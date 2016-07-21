ee9c80eca0eac4e8b3c04f43e5b32c5bd8a3fc12
remi.passmoilesel <remi.passmoilesel@gmail.com>
2016-07-21 11:39:00 +0200

    First operationnal screen capture extension

---
+++ screen-capture/chrome-extension/background-script.js
@@ -0,0 +1,84 @@
+﻿/**
+ *
+ * Background script for Djoe screen capture extension
+ *
+ * Executed in background. Ask for media and return it.
+ *
+ *
+ * @param message
+ */
+
+var config = {
+  debug : true, messagePrefix : "djoe.screencapture-extension."
+}
+
+/**
+ * Different messages can be sent or received
+ * @type {string}
+ */
+var messages = {
+  isAvailable : config.messagePrefix + "is-available",
+  available : config.messagePrefix + "available",
+  getScreenSourceId : config.messagePrefix + "get-screen-source-id",
+  getAPTSourceId : config.messagePrefix + "get-audio-plus-tab-source-id"
+};
+
+/**
+ * Log function
+ * @param message
+ */
+function log(message) {
+
+  if (config.debug !== true) {
+    return;
+  }
+
+  console.log("[CHROME-EXT CTN] " + message);
+  for (var i = 1; i < arguments.length; i++) {
+    console.log(arguments[i]);
+  }
+}
+log("Background script desktop capture extension");
+
+// default screen options
+var screenOptions = ['screen', 'window'];
+
+/**
+ * Listen messages from content script
+ */
+chrome.runtime.onConnect.addListener(function(port) {
+
+  port.onMessage.addListener(function(message) {
+
+    // someone request screen
+    if (message === messages.getScreenSourceId) {
+      chrome.desktopCapture.chooseDesktopMedia(screenOptions, port.sender.tab, onAccessApproved);
+    }
+
+    // someone tab and audio
+    else if (message === messages.getAPTSourceId) {
+      screenOptions = ['audio', 'tab'];
+      chrome.desktopCapture.chooseDesktopMedia(screenOptions, port.sender.tab, onAccessApproved);
+    }
+
+    else {
+      log("Unknown message: ", message);
+    }
+
+  });
+
+  // on getting sourceId
+  // "sourceId" will be empty if permission is denied.
+  function onAccessApproved(sourceId) {
+    // if "cancel" button is clicked
+    if (!sourceId || !sourceId.length) {
+      return port.postMessage('PermissionDeniedError');
+    }
+
+    // "ok" button is clicked; share "sourceId" with the
+    // content-script which will forward it to the webpage
+    port.postMessage({
+      sourceId : sourceId
+    });
+  }
+});
---
+++ screen-capture/chrome-extension/content-script.js
@@ -0,0 +1,113 @@
+﻿/**
+ *
+ * Content script for Djoe screen capture extension
+ *
+ * Executed in page context. Just relay messages to the background script
+ *
+ *
+ *
+ * @param message
+ */
+
+var config = {
+  debug : true, messagePrefix : "djoe.screencapture-extension."
+}
+
+/**
+ * Different messages can be sent or received
+ * @type {string}
+ */
+var messages = {
+  isAvailable : config.messagePrefix + "is-available",
+  available : config.messagePrefix + "available",
+  getScreenSourceId : config.messagePrefix + "get-screen-source-id",
+  getAPTSourceId : config.messagePrefix + "get-audio-plus-tab-source-id"
+};
+
+/**
+ * Log function
+ * @param message
+ */
+function log(message) {
+
+  if (config.debug !== true) {
+    return;
+  }
+  
+  console.log("[CHROME-EXT CTN] " + message);
+  for (var i = 1; i < arguments.length; i++) {
+    console.log(arguments[i]);
+  }
+}
+log("Initializing Chrome desktop capture extension");
+
+/**
+ * Return true if the message concern extension
+ */
+function filterMessage(message) {
+
+  if (typeof message !== "string") {
+    return false;
+  }
+
+  for (var key in messages) {
+    if (!messages.hasOwnProperty(key)) {
+      continue;
+    }
+
+    var value = messages[key];
+
+    if (message === value) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+/**
+ * Connexion with background script
+ */
+var port = chrome.runtime.connect();
+
+/**
+ * Forward messages from background script to web page
+ */
+port.onMessage.addListener(function(message) {
+  window.postMessage(message, '*');
+});
+
+/**
+ * Forward messages from webpage to background script
+ */
+window.addEventListener('message', function(event) {
+
+  log("Event received: ", event);
+
+  // if invalid source
+  if (event.source != window) {
+    log("Invalid message");
+    return;
+  }
+
+  var message = event.data;
+
+  // filter messages
+  if (filterMessage(message) !== true) {
+    log("Invalid message", event.data);
+    return;
+  }
+
+  // someone ask if extension is available
+  if (message === messages.isAvailable) {
+    window.postMessage(messages.available, "*");
+    return;
+  }
+
+  // otherwise transfer message to background script
+  port.postMessage(message);
+
+});
+
+// inform browser that extension is available
+window.postMessage(messages.available, '*');
