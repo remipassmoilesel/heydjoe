fdfaac8875817228a1077632ef45d1001da83e4a
remi.passmoilesel <remi.passmoilesel@gmail.com>
2016-07-21 12:11:06 +0200

    Add configuration file

--- utils/grunt-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# mardi 31 mai 2016, 10:43:27 (UTC+0200)
-
-export PATH=$PATH:/home/remipassmoilesel/nodejs4/bin && cd var/www/djoe/jsxc/ && grunt build:release
-
-# Notification d'arrêt en cas d'erreur
-alert 'Fin de grunt !'
