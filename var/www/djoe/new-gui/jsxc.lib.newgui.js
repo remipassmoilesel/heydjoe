822fc16ee716f0383952b85692a4ef3b2ede9b33
remi.passmoilesel <remi.passmoilesel@gmail.com>
2016-08-14 12:34:31 +0200

    Stable version before new gui integration

--- var/www/djoe/new-gui/jsxc.lib.newgui.js
+++ var/www/djoe/new-gui/jsxc.lib.newgui.js
@@ -1,6 +1,3 @@
-// TODO: onSlave
-// TODO: ...
-
 /**
  *
  *
@@ -11,6 +8,10 @@ var jsxc = {
    * @memberOf jsxc
    */
   newgui : {
+    
+    // TODO: onSlave
+    // TODO: onHTTP/HTTPS
+    // TODO: onLocalStorageUnavailable...
 
     /**
      * Sidebar of deployed chat sidebar
@@ -26,7 +27,9 @@ var jsxc = {
 
     SCROLL_ANIMATION_DURATION : '500',
 
-    MENU_APPEAR_ANIMATION_DURATION : '800',
+    FLOATING_MENU_ANIMATION_DURATION : '800',
+
+    OPACITY_ANIMATION_DURATION : '500',
 
     _log : function(message, data, level) {
 
@@ -48,19 +51,14 @@ var jsxc = {
        * Header: Always visible
        *
        */
-      var header = $("#jsxc-chat-sidebar-header");
-      header.click(function() {
-
-      });
-
       // open and close chat sidebar
-      var togglechat = $("#jsxc-chat-sidebar-header .jsxc-toggle-chat");
+      var togglechat = $("#jsxc-chat-sidebar-header .jsxc-toggle-sidebar");
       togglechat.click(function() {
         self.toggleChatSidebar();
       });
 
       // open and close video panel
-      var togglevideo = $("#jsxc-chat-sidebar-header .jsxc-toggle-video");
+      var togglevideo = $("#jsxc-chat-sidebar-header .jsxc-toggle-mediapanel");
       togglevideo.click(function() {
         self.toggleMediapanel();
       });
@@ -93,14 +91,41 @@ var jsxc = {
         self.toggleChatSidebar();
       });
 
+      // close media panel
+      $("#jsxc-mediapanel .jsxc-close-mediapanel").click(function() {
+        self.toggleMediapanel();
+      });
+
       // settings menu
       self._initSettingsMenu();
 
       // actions
       self._initActionsMenu();
 
+      self._initSearchBar();
+
       // optionnal
       // self.initMediaPanelMouseNavigation();
+
+    },
+
+    _initSearchBar : function() {
+
+      var self = jsxc.newgui;
+
+      $(".jsxc-action_search-user").click(function() {
+        self.chatSidebarContent.showContent('jsxc-search-users');
+      });
+
+      var searchBar = $('jsxc-chat-sidebar-search');
+
+      searchBar.keyup(function() {
+
+        var val = searchBar.val();
+
+        self.filterBuddies(val);
+
+      });
 
     },
 
@@ -202,6 +227,14 @@ var jsxc = {
 
     },
 
+    /**
+     * Buddylist filter. Allow to show only buddies or conversations.
+     *
+     * For historical reasons, buddies and conversations are stored in same
+     * containers.
+     *
+     * @param mode
+     */
     toggleBuddyFilter : function(mode) {
 
       var self = jsxc.newgui;
@@ -215,17 +248,48 @@ var jsxc = {
       // TODO check how was selected buddies in original JSXC
       var list = self._getBuddyList();
 
-      // self._log("buddylist", list);
+      // hide all
+      list.each(function() {
+        $(this).css({
+          'opacity' : 0
+        });
+      });
 
-      var displayBuddies = mode === 'buddies' ? 'block' : 'none';
-      var displayConversations = displayBuddies === 'block' ? 'none' : 'block';
+      var hideElement = function(element) {
+        element.css("display", "none");
+      };
+
+      var showElement = function(element) {
+
+        element.css({
+          display : 'block'
+        });
+
+        element.animate({
+          opacity : 1
+        }, self.OPACITY_ANIMATION_DURATION);
+
+      };
+
+      var applyBuddie = mode === 'buddies' ? showElement : hideElement;
+      var applyConversation = mode === 'conversations' ? hideElement : showElement;
 
       list.each(function() {
         var element = $(this);
 
-        element.css('display',
-            element.data('type') === 'chat' ? displayBuddies : displayConversations);
+        if (element.data('type') === 'chat') {
+          applyBuddie(element);
+        } else {
+          applyConversation(element);
+        }
+
       });
+
+      // show buddy panel if necessary
+      // at end of all treatments !
+      if (self.chatSidebarContent.isMainContentVisible() !== true) {
+        self.chatSidebarContent.showMainContent();
+      }
 
     },
 
@@ -482,7 +546,7 @@ var jsxc = {
         // animate openning
         menu.animate({
           opacity : 1
-        }, self.MENU_APPEAR_ANIMATION_DURATION, function() {
+        }, self.FLOATING_MENU_ANIMATION_DURATION, function() {
 
         });
 
@@ -495,7 +559,7 @@ var jsxc = {
         // animate closing
         menu.animate({
           opacity : 0
-        }, self.SIDEBAR_ANIMATION_DURATION, function() {
+        }, self.FLOATING_MENU_ANIMATION_DURATION, function() {
 
           // hide menu
           menu.css({
@@ -700,34 +764,50 @@ var jsxc = {
         var self = jsxc.newgui.chatSidebarContent;
         self._setContentVisible("jsxc-buddy-list-container");
       },
-      
-      toggleContent: function(contentId){
+
+      showContent : function(contentId) {
+        var self = jsxc.newgui.chatSidebarContent;
+        self._setContentVisible(contentId);
+      },
+
+      isMainContentVisible : function() {
+        return jsxc.newgui.chatSidebarContent.isContentVisible("jsxc-buddy-list-container");
+      },
+
+      isContentVisible : function(contentId) {
+
+        var element = $("#" + contentId);
+
+        if (element.length < 1) {
+          throw new Error("Unable to find: " + contentId);
+        }
+
+        return element.css("display") === "block";
+
+      },
+
+      toggleContent : function(contentId) {
 
         var self = jsxc.newgui.chatSidebarContent;
 
         // check if content shown
-        var visible = $("#" + contentId).css("display") === "block";
+        var visible = self.isContentVisible(contentId);
 
-        if(visible){
+        if (visible) {
           self.showMainContent();
         }
 
         else {
           self._setContentVisible(contentId);
         }
-        
+
       },
-      
-      _setContentVisible: function(contentId){
-        
+
+      _setContentVisible : function(contentId) {
+
         var self = jsxc.newgui.chatSidebarContent;
 
-        // get all panels availables
-        var contents = self.getAllContents();
-
-        var found = false;
-
-        self.getAllContents().each(function(){
+        self.getAllContents().each(function() {
           var element = $(this);
           $(this).css('display', 'none');
         });
@@ -735,24 +815,22 @@ var jsxc = {
         // the corresponding panel we have to show after hiding the visible
         var toDisplay = $('#jsxc-sidebar-content-viewport #' + contentId);
 
-        if(toDisplay.length < 1){
+        if (toDisplay.length < 1) {
           throw new Error("Unable to find ID: " + contentId);
-        }
-        else {
-          
+        } else {
+
           // display element transparent
           toDisplay.css({
-            opacity: 0,
-            display: 'block'
+            opacity : 0, display : 'block'
           });
-          
+
           // animate opacity
           toDisplay.animate({
-            opacity: 1
-          }, self.MENU_APPEAR_ANIMATION_DURATION);
-          
+            opacity : 1
+          }, self.OPACITY_ANIMATION_DURATION);
+
         }
-        
+
       }
 
     }
