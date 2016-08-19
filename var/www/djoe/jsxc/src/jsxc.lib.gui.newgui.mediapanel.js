ae45b4a7da32b1b7b7c079fa5cf5225505e54e61
remi.passmoilesel <remi.passmoilesel@gmail.com>
2016-08-19 08:58:36 +0200

    Refactoring newgui to divide it in several modules

---
+++ var/www/djoe/jsxc/src/jsxc.lib.gui.newgui.mediapanel.js
@@ -0,0 +1,188 @@
+/**
+ * All stuff needed by media panel, the folding panelon top of window
+ */
+$.extend(jsxc.newgui, {
+
+  /**
+   * Return an JQuery object selecting all media ressources displayed
+   * @returns {*|JQuery|jQuery|HTMLElement}
+   */
+  getAllDisplayedMediaRessource : function() {
+    return $("#jsxc-mediapanel .jsxc-media-ressource");
+  },
+
+  /**
+   * Toggle video panel
+   *
+   * If video panel have to be shown, chat sidebar have too
+   *
+   * @param callbackWhenFinished
+   */
+  toggleMediapanel : function(callbackWhenFinished) {
+
+    var self = jsxc.newgui;
+
+    var mediapanel = $("#jsxc-mediapanel");
+
+    if (self.isMediapanelShown() === false) {
+
+      mediapanel.find(".jsxc-close-mediapanel").css({
+        display : 'block'
+      });
+
+      // add box shadow
+      mediapanel.css("box-shadow", "3px 3px 3px 3px rgba(0, 0, 0, 0.1)");
+
+      mediapanel.animate({
+        height : self.MEDIAPANEL_HEIGHT
+      }, self.SIDEBAR_ANIMATION_DURATION, function() {
+
+        // Animation complete.
+        mediapanel.addClass("jsxc-deploy");
+
+        if (callbackWhenFinished) {
+          callbackWhenFinished();
+        }
+      });
+
+    }
+
+    else {
+
+      mediapanel.find(".jsxc-close-mediapanel").css({
+        display : 'none'
+      });
+
+      mediapanel.animate({
+        height : '0px'
+      }, self.SIDEBAR_ANIMATION_DURATION, function() {
+
+        // Animation complete.
+        mediapanel.removeClass("jsxc-deploy");
+
+        // remove box shadow
+        mediapanel.css("box-shadow", "none");
+
+        if (callbackWhenFinished) {
+          callbackWhenFinished();
+        }
+      });
+
+    }
+  },
+
+  /**
+   * Return true if chat sidebar is shown
+   */
+  isMediapanelShown : function() {
+    return $("#jsxc-mediapanel").hasClass("jsxc-deploy");
+  },
+
+  /**
+   * Open a media ressource in the media panel
+   * @param ressource
+   */
+  openMediaRessource : function(ressource) {
+
+    var self = jsxc.newgui;
+    var ress = jsxc.ressources;
+
+    if (self.isMediapanelShown() === false) {
+      self.toggleMediapanel();
+    }
+
+    //retrieve prefix of ressource
+    var prefix = ressource.substring(0, ressource.indexOf(":"));
+
+    var ressourceOnly = ressource.substring(prefix.length + 1, ressource.length);
+
+    self._log("openMediaRessource: ", {
+      ressource : ressource, prefix : prefix, ressourceOnly : ressourceOnly
+    });
+
+    var embedded = ress.getEmbeddedFor(prefix, ressourceOnly);
+
+    // add ressource only if needed
+    if (embedded) {
+      self.addMediaRessource(embedded, "Vidéo: " + ressourceOnly);
+    }
+
+  },
+
+  /**
+   * Remove a media ressource
+   * @param container
+   */
+  removeMediaRessource : function(container) {
+
+    var self = jsxc.newgui;
+
+    if (!container) {
+      throw new Error("Invalid argument: " + container);
+    }
+
+    container.animate({
+      opacity : "0"
+    }, self.OPACITY_ANIMATION_DURATION, function() {
+      container.remove();
+    });
+
+  },
+
+  /**
+   * Add a ressource in media panel, wrapped in container
+   *
+   * @param htmlContent
+   * @param title
+   * @param ressource
+   * @private
+   */
+  addMediaRessource : function(htmlContent, title, options) {
+
+    var self = jsxc.newgui;
+
+    var defaultOptions = {
+      /**
+       * Controls availables next the title
+       *
+       * If null, a close cross will be happend
+       */
+      titleControls : null
+    };
+
+    options = $.extend(defaultOptions, options);
+
+    // container for ressource
+    var container = $('<div class="jsxc-media-ressource"></div>').append(htmlContent);
+
+    // displayable title, not too long
+    var dspTitle = title.length > 30 ? title.substring(0, 27) + "..." : title;
+
+    // header with title and close cross
+    var ressHeader = $("<h1 class='jsxc-title'>" + dspTitle + "</h1>").attr('title', title);
+    container.prepend(ressHeader);
+
+    // add close control next the title
+    if (!options.titleControls) {
+
+      var closeHeader = $("<span class='jsxc-close-ressource'></span>");
+      closeHeader.click(function() {
+        self.removeMediaRessource(container);
+      });
+
+      ressHeader.append(closeHeader);
+    }
+
+    // user provide custom controls, add them
+    else {
+      ressHeader.append(options.titleControls);
+    }
+
+    self._log("addMediaRessource", {title : title, container : container});
+
+    // append ressource
+    $("#jsxc-mediapanel-right").append(container);
+
+  },
+
+});
---
+++ var/www/djoe/jsxc/src/jsxc.lib.gui.newgui.sidebar.js
@@ -0,0 +1,652 @@
+/**
+ * All stuff needed by chat sidebar, displayed at the bottom right of screen
+ */
+$.extend(jsxc.newgui, {
+
+  /**
+   * Management the middle of the chat sidebar, to display of remove several contents
+   * @memberOf jsxc.newgui
+   */
+  chatSidebarContent : {
+
+    /**
+     * Return a find search resuts with all possible displayable viewports
+     * @returns {*|jQuery}
+     */
+    getAllContents : function() {
+      return $('#jsxc-sidebar-content-viewport').find(".jsxc-viewport-content");
+    },
+
+    showMainContent : function() {
+      var self = jsxc.newgui.chatSidebarContent;
+      self._setContentVisible("jsxc-buddy-list-container");
+    },
+
+    showContent : function(contentId) {
+      var self = jsxc.newgui.chatSidebarContent;
+      self._setContentVisible(contentId);
+    },
+
+    isMainContentVisible : function() {
+      return jsxc.newgui.chatSidebarContent.isContentVisible("jsxc-buddy-list-container");
+    },
+
+    isContentVisible : function(contentId) {
+
+      var element = $("#" + contentId);
+
+      if (element.length < 1) {
+        throw new Error("Unable to find: " + contentId);
+      }
+
+      return element.css("display") === "block";
+
+    },
+
+    toggleContent : function(contentId) {
+
+      var self = jsxc.newgui.chatSidebarContent;
+
+      // check if content shown
+      var visible = self.isContentVisible(contentId);
+
+      if (visible) {
+        self.showMainContent();
+      }
+
+      else {
+        self._setContentVisible(contentId);
+      }
+
+    },
+
+    _setContentVisible : function(contentId) {
+
+      var self = jsxc.newgui.chatSidebarContent;
+
+      self.getAllContents().each(function() {
+        $(this).css('display', 'none');
+      });
+
+      // the corresponding panel we have to show after hiding the visible
+      var toDisplay = $('#jsxc-sidebar-content-viewport #' + contentId);
+
+      if (toDisplay.length < 1) {
+        throw new Error("Unable to find ID: " + contentId);
+      } else {
+
+        // display element transparent
+        toDisplay.css({
+          opacity : 0, display : 'block'
+        });
+
+        // animate opacity
+        toDisplay.animate({
+          opacity : 1
+        }, self.OPACITY_ANIMATION_DURATION);
+
+      }
+
+    }
+
+  },
+
+  /**
+   * Update the name displayed on bottom of the chat sidebar
+   */
+  updateStatusBarUserName : function() {
+
+    if (jsxc.xmpp.conn) {
+      $("#jsxc-status-bar .jsxc-user-name").text(Strophe.getNodeFromJid(jsxc.xmpp.conn.jid));
+    }
+
+    else {
+      $("#jsxc-status-bar .jsxc-user-name").text("Déconnecté");
+    }
+
+  },
+
+  /**
+   * Notifications panel, where are displayed all notifications
+   * @private
+   */
+  _initNotificationsPanel : function() {
+
+    var self = jsxc.newgui;
+
+    // add openning action
+    $("#jsxc-actions-menu .jsxc-action_manage-notifications").click(function() {
+      self.toggleNotificationsMenu();
+    });
+
+  },
+
+  /**
+   * Update the top of the chat sidebar to display notices or others
+   */
+  updateChatSidebarHeader : function(disconnected) {
+
+    var self = jsxc.newgui;
+
+    var headerContent = $("#jsxc-chat-sidebar-header .jsxc-header-content");
+    headerContent.empty();
+    headerContent.off('click');
+
+    // we are disconnected
+    if (disconnected === true) {
+      headerContent.append('<span>Déconnecté</span>');
+    }
+
+    // if notifications, display them
+    else if (jsxc.notice.getNotificationsNumber() > 0) {
+
+      headerContent.append(
+          '<span><span class="jsxc_menu_notif_number"></span> notification(s)</span>');
+
+      // open notifications on click
+      headerContent.click(function(event) {
+        event.stopPropagation();
+
+        if (self.isChatSidebarShown() !== true) {
+          self.toggleChatSidebar();
+        }
+        self.toggleNotificationsMenu();
+
+      });
+
+      jsxc.notice.updateNotificationNumbers();
+
+    }
+
+    // if not, display online buddies
+    else {
+      var online = $('#jsxc_buddylist li[data-status!="offline"][data-type="chat"]').length;
+      headerContent.append('<span>' + online + ' personne(s) en ligne</span>');
+    }
+
+    // keep handler if used like this
+    return true;
+
+  },
+
+  /**
+   * Update user indicator in the bottom of chat sidebar
+   */
+  updateOwnPresenceIndicator : function(disconnection) {
+
+    var statusSelect = $("#jsxc-status-bar .jsxc-select-status");
+    var username = $('#jsxc-status-bar .jsxc-user-name');
+    var pres = jsxc.storage.getUserItem('presence') || 'online';
+    var selectedPres = statusSelect ? statusSelect.val() : null;
+
+    username.removeClass('jsxc_online jsxc_chat jsxc_away jsxc_xa jsxc_dnd jsxc_offline');
+
+    // we are connected
+    if (jsxc.xmpp.conn && disconnection !== true) {
+
+      // change icon of user name
+      username.addClass('jsxc_' + pres);
+
+      if (statusSelect) {
+
+        // check if status was not changed programmatically
+        if (statusSelect.attr("disabled") === "disabled") {
+          statusSelect.attr('disabled', false);
+        }
+
+        if (selectedPres !== pres) {
+          statusSelect.val(pres);
+        }
+
+      }
+
+    }
+
+    // we are disconnected
+    else {
+      username.addClass('jsxc_offline');
+      username.addClass('user');
+
+      if (statusSelect) {
+        statusSelect.val("novalue");
+        statusSelect.attr("disabled", true);
+      }
+
+    }
+
+  },
+
+  /**
+   * Search user panel (XEP 0055)
+   * @private
+   */
+  _initSearchPanel : function() {
+
+    var self = jsxc.newgui;
+
+    $(".jsxc-action_search-user").click(function() {
+      self.chatSidebarContent.showContent('jsxc-search-users');
+    });
+
+    var searchBar = $('#jsxc-chat-sidebar-search');
+
+    searchBar.keyup(function() {
+
+      var terms = searchBar.val();
+
+      clearTimeout(self._searchTimer);
+      self._searchTimer = setTimeout(function() {
+
+        console.info("Search: " + terms);
+
+        jsxc.xmpp.search.searchUsers(terms).then(function(results) {
+          self._displayUserSearchResults(results);
+        }).fail(function(error) {
+          self._displayUserSearchError(error);
+        });
+
+      }, 700);
+
+    });
+
+  },
+
+  /**
+   * Called when we have to display search user results. Results can be selected to invite buddies.
+   * @param results
+   * @private
+   */
+  _displayUserSearchResults : function(results) {
+
+    var list = $("#jsxc-chat-sidebar .jsxc-search-users-results");
+
+    list.empty();
+
+    var displayed = 0;
+    var ownJid = Strophe.getBareJidFromJid(jsxc.xmpp.conn.jid);
+
+    $.each(results, function(index, element) {
+
+      if (element.jid === ownJid) {
+        // do not display but continue
+        return true;
+      }
+
+      var res = $("<div class='jsxc-search-user-entry'></div>").text(element.username);
+      res.css({
+        display : 'block', opacity : 0
+      });
+
+      res.data('jid', element.jid);
+
+      // element to show is a buddy, an special icon is displayed and switched with checked icon on
+      // selection
+      if (element._is_buddy === true) {
+        res.attr('title', element.username + " est dans vos contacts");
+        res.addClass("jsxc-search-result-buddie");
+        res.click(function() {
+
+          if (res.hasClass("jsxc-search-result-buddie")) {
+            res.removeClass("jsxc-search-result-buddie");
+            res.addClass("jsxc-checked");
+          } else {
+            res.removeClass("jsxc-checked");
+            res.addClass("jsxc-search-result-buddie");
+          }
+
+        });
+      }
+
+      // element to show is not a buddy
+      else {
+        res.attr('title', element.username + " n'est pas dans vos contacts");
+        res.click(function() {
+          res.toggleClass("jsxc-checked");
+        });
+      }
+
+      list.append(res);
+
+      res.animate({
+        'opacity' : 1
+      });
+
+      displayed++;
+    });
+
+    if (displayed < 1) {
+      list.append("<div class='jsxc-search-user-entry'>Aucun résultat</div>");
+      return;
+    }
+
+  },
+
+  /**
+   * Called when an error occur while searching in user list
+   *
+   * @param error
+   * @private
+   */
+  _displayUserSearchError : function(error) {
+
+    var list = $("#jsxc-chat-sidebar-search .jsxc-search-users-results");
+
+    list.empty();
+
+    list.append("<div>Erreur lors de la recherche: " + error + "</div>");
+
+  },
+
+  /**
+   * Open or close notifications panem
+   */
+  toggleNotificationsMenu : function() {
+    jsxc.newgui.chatSidebarContent.toggleContent('jsxc-manage-notifications');
+  },
+
+  /**
+   * Open or close settings menu
+   */
+  toggleActionsMenu : function() {
+    jsxc.newgui.chatSidebarContent.toggleContent('jsxc-actions-menu');
+  },
+
+  /**
+   * EXPERIMENTAL
+   * Allow users to navigate on mediapanel without scrollbars
+   */
+  initMediaPanelMouseNavigation : function() {
+
+    // var self = jsxc.newgui;
+
+    var mpanel = $("#jsxc-mediapanel-right");
+    var lastMove;
+
+    mpanel.mousemove(function(event) {
+
+      console.log(event.pageX, event.pageY);
+
+      // do not operate if mouse too down
+      if (event.pageY > 150) {
+        return true;
+      }
+
+      // first move
+      if (!lastMove) {
+        lastMove = event.pageX;
+
+        // keep handler
+        return true;
+      }
+
+      // get dimensions of panel
+      var frameSize = mpanel.width();
+      var viewportSize = 0;
+      mpanel.find("div.jsxc-media-ressource").each(function() {
+        viewportSize += $(this).width();
+      });
+
+      if (viewportSize > frameSize) {
+
+        var factor = viewportSize / frameSize;
+
+        // get direction
+        var direction = lastMove - event.pageX;
+
+        mpanel.scrollLeft(mpanel.scrollLeft() - direction * factor);
+
+      }
+
+      lastMove = event.pageX;
+
+    });
+
+    // // Optionnal: create fake ressources
+    // for (var i = 0; i < 10; i++) {
+    //   self.addMediaRessource(
+    //       "<div style='background: red; margin: 20px; width: 400px; height: 400px'></div>",
+    //       'Title ' + i);
+    // }
+
+  },
+
+  /**
+   * Buddylist filter. Allow to show only buddies or conversations.
+   *
+   * For historical reasons, buddies and conversations are stored in same
+   * containers.
+   *
+   * @param mode
+   */
+  toggleBuddyFilter : function(mode) {
+
+    var self = jsxc.newgui;
+    var roster = jsxc.gui.roster;
+
+    self._log("toggleBuddyFilter: " + mode);
+
+    // set filter for future adding
+    roster.setFilterMode(mode);
+
+    // TODO check how was selected buddies in original JSXC
+    var list = self._getBuddyList();
+
+    // hide all
+    list.each(function() {
+      $(this).css({
+        'opacity' : 0
+      });
+    });
+
+    var hideElement = function(element) {
+      element.css("display", "none");
+    };
+
+    var showElement = function(element) {
+
+      element.css({
+        display : 'block'
+      });
+
+      element.animate({
+        opacity : 1
+      }, self.OPACITY_ANIMATION_DURATION);
+
+    };
+
+    var applyBuddie = mode === 'buddies' ? showElement : hideElement;
+    var applyConversation = mode === 'conversations' ? showElement : hideElement;
+
+    list.each(function() {
+      var element = $(this);
+
+      if (element.data('type') === 'chat') {
+        applyBuddie(element);
+      } else {
+        applyConversation(element);
+      }
+
+    });
+
+    // show buddy panel if necessary
+    // at end of all treatments !
+    if (self.chatSidebarContent.isMainContentVisible() !== true) {
+      self.chatSidebarContent.showMainContent();
+    }
+
+  },
+
+  toggleSelectionMode : function() {
+
+    var self = jsxc.newgui;
+
+    self._log("toggleSelectionMode: " + self._selectionMode);
+
+    var list = self._getBuddyList();
+
+    self._selectionMode = !self._selectionMode;
+
+    if (self._selectionMode === false) {
+
+      // remove all click handler and replace it by selector
+      list.each(function() {
+
+        var element = $(this);
+        element.off('click');
+
+        element.on('click', function() {
+          var toDecorate = $(this).find('div.jsxc_name');
+          self._toggleBuddySelected(toDecorate);
+        });
+
+      });
+    }
+
+    else {
+
+      // remove all click handler and replace it by selector
+      list.each(function() {
+
+        var element = $(this);
+        element.off('click');
+
+        element.find(".jsxc-checked").removeClass("jsxc-checked");
+
+        element.click(function() {
+          jsxc.gui.window.open(element.data("bid"));
+        });
+
+      });
+    }
+
+  },
+
+  _toggleBuddySelected : function(toDecorate) {
+
+    var className = 'jsxc-checked';
+    if (toDecorate.hasClass(className)) {
+      toDecorate.removeClass(className);
+    } else {
+      toDecorate.addClass(className);
+    }
+    return toDecorate;
+  },
+
+  /**
+   * Return an JQuery instance of the buddy list ( li )
+   * @returns {*|JQuery|jQuery|HTMLElement}
+   * @private
+   */
+  _getBuddyList : function() {
+
+    // TODO check how was selected buddies in original JSXC
+    return $("#jsxc_buddylist li.jsxc_rosteritem");
+
+  },
+
+  /**
+   * Return true if chat sidebar is shown
+   */
+  isChatSidebarShown : function() {
+    return $("#jsxc-chat-sidebar-content").hasClass("jsxc-deploy");
+  },
+
+  /**
+   * Open or close settings menu
+   */
+  toggleSettingsMenu : function() {
+    jsxc.newgui.chatSidebarContent.toggleContent('jsxc-settings-menu');
+  },
+
+  /**
+   * Show / hide the panel where we can search users
+   */
+  toggleSearchPanel : function() {
+
+    var self = jsxc.newgui;
+
+    if (self.chatSidebarContent.isContentVisible('jsxc-search-users') !== true) {
+      self.chatSidebarContent.showContent('jsxc-search-users');
+    } else {
+      self.chatSidebarContent.showMainContent();
+    }
+
+  },
+
+  /**
+   * Show / hide the chat sidebar
+   */
+  toggleChatSidebar : function(callbackWhenFinished) {
+
+    var self = jsxc.newgui;
+
+    var content = $("#jsxc-chat-sidebar-content");
+    var settings = $("#jsxc-chat-sidebar .jsxc-toggle-settings");
+    var closeCross = $('#jsxc-chat-sidebar .jsxc-close-chatsidebar');
+
+    if (self.isChatSidebarShown() === false) {
+
+      // show close button
+      closeCross.css({
+        opacity : '0', display : 'inline-block'
+      });
+      closeCross.animate({
+        opacity : 1
+      });
+
+      // show settings button
+      settings.css({
+        opacity : '0', display : 'inline-block'
+      });
+      settings.animate({
+        opacity : 1
+      });
+
+      // raise sidebar
+      content.animate({
+        height : self.SIDEBAR_CONTENT_HEIGHT
+      }, self.SIDEBAR_ANIMATION_DURATION, function() {
+
+        // Animation complete.
+        content.addClass("jsxc-deploy");
+
+        if (callbackWhenFinished) {
+          callbackWhenFinished();
+        }
+      });
+
+    }
+
+    else {
+
+      // hide close button
+      closeCross.animate({
+        opacity : 0
+      });
+
+      // hide settings button
+      settings.animate({
+        opacity : 0
+      });
+
+      // drop down sidebar
+      content.animate({
+        height : '0px'
+      }, self.SIDEBAR_ANIMATION_DURATION, function() {
+
+        // Animation complete.
+        content.removeClass("jsxc-deploy");
+
+        settings.css({
+          display : 'none'
+        });
+
+        if (callbackWhenFinished) {
+          callbackWhenFinished();
+        }
+      });
+
+    }
+
+  }
+});
